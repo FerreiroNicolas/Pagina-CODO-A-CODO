68da6ee8dee7e5ff44ea17d0f2148639f4f22a07
Nicolas Ferreiro <ferreiro.nico1@gmail.com>
2023-10-24 16:40:47 -0300

    nuevos cambios

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+import * as Vue from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import {mainCore} from "./mainCore.Vue.js";
+
+// TOMO EL MODULO A TRABAJAR Y CREAR LA APP
+const { createApp} = Vue; 
+
+// GUARDO EL ID EN UNA VARIABLE
+const mainId = "#form";
+const mainApp = createApp(mainCore);
+
+mainApp.mount(mainId);
