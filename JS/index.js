b96758f8964a9b7979df4b4c123693d7ec9a73f0
Nicolas Ferreiro <ferreiro.nico1@gmail.com>
2023-10-24 23:46:51 -0300

    Cambios nuevos

--- JS/index.js
+++ JS/index.js
@@ -5,12 +5,11 @@ $form.addEventListener('submit', handleSubmit);
 async function handleSubmit(event) {
     event.preventDefault();
 
-    // Ensure that you are passing the correct HTML form element to FormData.
-    const form = new FormData($form); // Change 'this' to '$form'.
+    const form = new FormData($form); 
 
     try {
-        const response = await fetch($form.action, { // Change 'this.action' to '$form.action'.
-            method: $form.method, // Change 'this.method' to '$form.method'.
+        const response = await fetch($form.action, { 
+            method: $form.method, 
             body: form,
             headers: {
                 'Accept': 'application/json'
@@ -18,13 +17,12 @@ async function handleSubmit(event) {
         });
 
         if (response.ok) {
-            $form.reset(); // Change 'this.reset()' to '$form.reset()'.
+            $form.reset(); 
             alert('Gracias por contactarnos, te escribiremos pronto');
         } else {
-            // Handle the case when the response is not OK (e.g., error handling).
+
         }
     } catch (error) {
-        // Handle errors that may occur during the fetch operation.
         console.error(error);
     }
 }
