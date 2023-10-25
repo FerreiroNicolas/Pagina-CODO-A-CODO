dff7d5b40adae3ddc8c9e357349b2c9d628de998
Nicolas Ferreiro <ferreiro.nico1@gmail.com>
2023-10-25 00:59:03 -0300

    nuevos cambios

--- JS/index.js
+++ JS/index.js
@@ -21,7 +21,7 @@ async function handleSubmit(event) {
             alert('Gracias por contactarnos, te escribiremos pronto');
         } else {
 
-        }
+    }
     } catch (error) {
         console.error(error);
     }
